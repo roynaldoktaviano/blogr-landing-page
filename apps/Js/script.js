a7472677c68e7314f08645fa3d3da13dca167c93
Roynald Oktaviano <roynald.okta@gmail.com>
2021-03-26 10:04:37 +0700

    Header Link Script Problem

--- apps/Js/script.js
+++ apps/Js/script.js
@@ -1,13 +1,8 @@
-const headerLink = document.querySelectorAll(".link");
-const headerBox = document.querySelector('.box');
+const headerLink = document.querySelectorAll('.link-content');
+const box = document.querySelector('.box');
 
 headerLink.forEach(function(elem){
-    const boxMenu = headerBox.parentElement.lastChild;
-    if(boxMenu.classList.contains('pop-in')){
-        boxMenu.classList.remove('pop-in');
-        boxMenu.classList.add('pop-out');
-    } else {
-        boxMenu.classList.remove('pop-out');
-        boxMenu.classList.add('pop-in');
+    if(elem.classList.contains('hide')){
+        
     }
-})
+}) 
