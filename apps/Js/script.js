46147e9a1d2206150a5ad2b8460ddb12ec366546
Roynald Oktaviano <roynald.okta@gmail.com>
2021-03-27 12:39:39 +0700

    Header Mobile Display

--- apps/Js/script.js
+++ apps/Js/script.js
@@ -1,8 +1,39 @@
 const headerLink = document.querySelectorAll('.link-content');
-const box = document.querySelector('.box');
+const hamburger = document.querySelector('.hamburger')
+const hamburgerLink = document.querySelectorAll('.hamburger-link');
 
 headerLink.forEach(function(elem){
-    if(elem.classList.contains('hide')){
-        
+    const boxFile = elem.lastElementChild;
+    elem.addEventListener("mouseenter",function(){
+        boxFile.classList.remove('pop-out');
+        boxFile.classList.add('pop-in');
+    })
+
+    elem.addEventListener("mouseleave", function(){
+        boxFile.classList.add('pop-out');
+        boxFile.classList.remove('pop-in');
+    })
+})
+
+hamburger.addEventListener("click", function(){
+    if(hamburger.classList.contains('open')){
+        hamburger.classList.remove('open')
+    } else {
+        hamburger.classList.add('open');
     }
-}) 
+})
+
+hamburgerLink.forEach(function(elem){
+    const arrow = elem.childNodes;
+    const text = elem.firstElementChild;
+    elem.addEventListener("click", function(){
+        if(elem.classList.contains('open')){
+            elem.classList.remove('open');
+            text.style.color = "#1f3f5b"; 
+        } else {
+            elem.classList.add('open');
+            text.style.color = "gray"
+            arrow[2].style.transform = "rotate(180deg)";
+        }
+    })
+})
